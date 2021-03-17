f9ea5b449ce68e1e39848e69198a58dc38e714f4
JurenIvan <juren.ivan@gmail.com>
2021-03-17 15:48:01 +0100

    Initial commit

---
+++ lab1/src/main/java/hr/fer/zemris/Bridge.java
@@ -0,0 +1,7 @@
+package hr.fer.zemris;
+
+public class Bridge {
+    public static void main(String[] args) {
+        System.out.println("bridge");
+    }
+}
---
+++ lab1/src/main/java/hr/fer/zemris/Car.java
@@ -0,0 +1,7 @@
+package hr.fer.zemris;
+
+public class Car {
+    public static void main(String[] args) {
+        System.out.println("car");
+    }
+}
